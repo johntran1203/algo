f14f421feb0d24ffffbae55515b89988df0acf68
johntran <tranjohn1211@gmail.com>
2022-10-10 21:03:22 -0700

    work on factorial agian

--- fib.js
+++ fib.js
@@ -68,4 +68,14 @@ function fibonacci(n){
     return fib
 }
 
-console.log(fibonacci(7))
+// console.log(fibonacci(7))
+
+function factorial(n){
+    let result =1
+    for(let i=2; i<=n; i++){
+        result = result * i
+    }
+    return result
+}
+
+console.log(factorial(5))
